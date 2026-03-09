ed8ad56014b8fad5bdd83bcd6c25a43ddc8f78e7
kayceeumunna <kayceeku65@gmail.com>
2026-03-09 14:04:30 +0100

    Assignment for module 4 lesson 8

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World!')
+const day = 'Thursday';
+
+const position = 4;
+
+const output = "Today is "+ day +". it is the "+position+"th day of the week"
+
+console.log(output)
+
+
+
+
+
+
+
+
+
