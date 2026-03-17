42e9470fbe38137a8a82b561e2de18c79054fe82
kayceeumunna <kayceeku65@gmail.com>
2026-03-17 15:04:37 +0100

    Include task 13

---
+++ Module4A_javaScript/Tasks/task13.js
@@ -0,0 +1,9 @@
+const books = {
+    title: 'Kelechi Temple',
+    description: 'A story of Religion and Tough Times',
+    numberOfPages: 34,
+    author: 'Kelechi Umunna',
+    reading: 'True'
+
+}
+console.log(books.numberOfPages)
